eb5c1dccf1e7c255428b7570c27a9ce391fa1e59
Saaappi <alex.metz1024@gmail.com>
2021-06-10 00:45:56 -0500

    Implemented a MUCH more efficient means of fetching anima count from the player's sanctum reservoir.

--- AnimaCounter.lua
+++ AnimaCounter.lua
@@ -11,7 +11,6 @@
 		t			: This is an empty table. This is how the addon can communicate between files or local functions, sort of like traditional classes.
 ]]--
 local addonName, t = ...;
-
 local levelRequiredForAnima = 60;
 
 BINDING_HEADER_ANIMACOUNTER = addonName;
@@ -28,25 +27,14 @@ function AnimaCounterCountAnima(key)
 					local quantity = GetItemCount(itemID, false); -- This will need to be multipled against the anima from the spell description.
 					local spell = Spell:CreateFromSpellID(spellID); -- GetSpellDescription isn't readily available on call, so create a spell object from the Spell Mixin. We'll get the description from that.
 					spell:ContinueOnSpellLoad(function()
-						local animaCountInDescription = string.match(spell:GetSpellDescription(), "%d+"); -- Extract the amount of anima from the spell's description.
-						currentAnimaCount = currentAnimaCount + (quantity * animaCountInDescription);
+						local animaCountInSpellDescription = string.match(spell:GetSpellDescription(), "%d+"); -- Extract the amount of anima from the spell's description.
+						currentAnimaCount = currentAnimaCount + (quantity * animaCountInSpellDescription);
 					end);
 				end
 			end
 		end
-		if (CharacterFrameTab3:IsVisible()) then
-			for i = 1, 10, 1 do
-				local text = _G["TokenFrameContainerButton"..i.."Name"]:GetText();
-				if (text == "Reservoir Anima") then
-					AnimaCounterAnimaInReservoir = tonumber(string.gsub(_G["TokenFrameContainerButton"..i.."Count"]:GetText(), "%,", ""));
-					break
-				end
-			end
-		end
-		if (AnimaCounterAnimaInReservoir ~= nil) then
-			print("|cffFFD839" .. addonName .. "|r: " .. currentAnimaCount .. " |cff5BA4E1Anima|r (" .. AnimaCounterAnimaInReservoir .. ") (" .. (currentAnimaCount + AnimaCounterAnimaInReservoir) .. ")");
-		else
-			print("|cffFFD839" .. addonName .. "|r: " .. currentAnimaCount .. " |cff5BA4E1Anima|r");
-		end
+		
+		AnimaCounterAnimaInReservoir = C_CurrencyInfo.GetCurrencyInfo(1813).quantity;
+		print("|cffFFD839" .. addonName .. "|r: " .. currentAnimaCount .. " |cff5BA4E1Anima|r (" .. (currentAnimaCount + AnimaCounterAnimaInReservoir) .. " |T3528288:0|t)");
 	end
 end
